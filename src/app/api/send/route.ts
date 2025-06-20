9d8258d517ee71525b300b02d0ed08758736327b
jpezuela <jorge.pezuela@hotmail.com>
2025-06-20 08:11:08 +0200

    devops-portfolio: Fix Resend flow

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Emanuel Peire <resend@emapeire.xyz>',
+      from: 'Contact Form <onboarding@resend.dev>',
       to: process.env.RESEND_EMAIL_TO || 'jorge.pezuela@hotmail.com',
       subject: 'Message from contact form',
       react: ContactEmailTemplate({
